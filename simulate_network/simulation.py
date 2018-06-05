ef9f10d19c687e819583c4b8d130caf148bb9c4a
di ma <dimitra.maoutsa@tu-berlin.de>
2018-06-05 19:16:42 +0000

    Update simulation.py

--- simulate_network/simulation.py
+++ simulate_network/simulation.py
@@ -14,11 +14,8 @@ the Nest-Simulator documentation:
 Output
 ----------
 "Data/delay.dat" :  Contains the synaptic delay employed in the simulations
-
 "Data/ex_neurons" : Contains the corresponding spiking times of excitatory neurons
-
 "Data/in_neurons" : Contains the corresponding spiking times of inhibitory neurons
-
 "Data/connectivity.dat" :  Contains the connecitivy matrix used in the simulation
 
 Accompanying material to "Inferring network connectivity from event timing
